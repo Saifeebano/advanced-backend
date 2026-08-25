0a8dfacaf705e2992037f1d7f79db398c17b9118
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2026-08-25 13:35:07 +0530

    add video jwt hook commit

---
+++ src/models/vedio.model.js
@@ -0,0 +1,51 @@
+import { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const vedioSchema = new Schema({
+
+    vediofile: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    duration: {
+        type: Number, //cloudnary m second mai aaega
+        required: true
+    },
+    view: {
+        type: Number,
+        default: 0
+    },
+    ispublished: {
+        type: Boolean,
+        default: true
+    },
+    publishAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+
+}, {
+    timestamps: true
+})
+
+vedioSchema.plugin(mongooseAggregatePaginate)
+
+
+export const video = mongoose.model("video", vedioSchema)
