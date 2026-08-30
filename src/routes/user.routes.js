9996c540edbe9759d8624bc28e17b7a737ab8887
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2026-08-30 15:01:59 +0530

    feat: implement user authentication routes and controllers with file upload and JWT support

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { refershAccessToken, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -23,6 +23,9 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("/refresh-access-token").post(refreshAccessToken)
+
 
 
 export default router
